054e330c50d94516ea6bc2638f5aeef3df94039a
Owais Idrisi <79826730+OWAISIDRISI53@users.noreply.github.com>
2022-04-30 00:33:39 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,10 @@
+let header = document.querySelector('header')
+let main = document.querySelector('.main')
+let about = document.querySelector('.about')
+let overlay = document.querySelector('.overlay')
+
+function show() {
+  header.classList.add('hide')
+  main.classList.add('hide')
+  about.classList.add('show')
+}
